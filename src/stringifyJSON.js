ad5eb77c9b512d298d39dd954678c9fa4205f1bd
Erin Kavanaugh <erin@erin-MBA.local>
2016-03-24 16:43:35 -0700

    StringifyJSON works for all values except functions and undefined

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -5,60 +5,33 @@
 
 var stringifyJSON = function(obj) {//How do I want to recur this. Each value needs to be an individual string. I could check type and then stringify. cycle thorugh every value and return the string version. 
   // your code goes here
-  var result = "";
-  var value; 
+	var theString = '';
+	if(Array.isArray(obj)) {
+		obj.forEach((item) => {
+			theString += stringifyJSON(item) + ",";
+		})
+		return "["+ theString.slice(0,-1)+"]";
+	}  else if (typeof obj === "object" && obj !== null) {
+		for(var key in obj) {
+			if(typeof obj[key] === "function" || obj === undefined) {
+				continue; 
+			}
+			theString += baseCase(key) + ":" + stringifyJSON(obj[key])+","; 
+		}
+		return "{"+ theString.slice(0,-1)+"}"; 
 
-  if(typeof obj !== "object") {
-  	console.log(result); 
-  	result = result.concat(obj.toString()); 
-  	console.log(result); 
-  	return result;
-  } else if (obj === null) {
-  	result = result.concat("null"); 
-  }else if (typeof obj === "function"||"undefined") {
-  	return;
-  } else {
-  	if(Array.isArray(obj)) {
-  		value = obj.shift;
-  		console.log(value); 
-  		console.log(result); 
-  		return stringifyJSON(value); 
-  	} else {
-  		for(value in obj) {
-  			console.log(result); 
-  			return stringifyJSON(obj[value]); 
-  		}
-  	}
-  }
-};
+	} 
+
+	return baseCase(obj); 
+}; 
+
+// "[9"Hello world"[][8][][8,"hi"][1,0,-1,-0.3,0.3,1343.32,3345,0.00011999999999999999],[8,[[],3,4]],[[[["foo"]]]],{},{"a":"apple"},{"foo":true,"bar":false,"baz":null},{"boolean, true":true,"boolean, false":false,"null":null},{"a":{"b":"c"}},{"a":["b","c"]}]
 
 
+function baseCase(val) {
+	if (typeof val === "string") {
+		return '"'+val+'"';
+	} 
+	return String(val); 
+}; 
 
- //  else if(typeof obj === "object") {
- //  		if (Array.isArray(obj))
-	//   	for(var key in obj) {
-	//   		if(typeof obj[key] === "function" || "undefined") continue; 
-	//   		else {
-	//   			console.log(result, obj[key]); 
-	//   		}
-	//   	}
-	// }
- //  }
- //  console.log(result);
-
-
-
-
-  // var value = obj.shift(); 
-  // for (var i = 0; i<obj.length; i++) {
-  // 	if(obj.length === 0) {
-  // 		return result; 
-  // } else {
-	 //  	result = result + obj[i].toString();
-	 //  	console.log(result);
-	 //  	return stringifyJSON(obj); 
-  // }
-  // }
-
-
-// "["Hello world",[],[8],[],[8,"hi"],[1,0,-1,-0.3,0.3,1343.32,3345,0.00011999999999999999],[8,[[],3,4]],[[[["foo"]]]],{},{"a":"apple"},{"foo":true,"bar":false,"baz":null},{"boolean, true":true,"boolean, false":false,"null":null},{"a":{"b":"c"}},{"a":["b","c"]}]"
