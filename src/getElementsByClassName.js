53dbbc050828433e02cb1080a35ec757ddd081aa
Erin Kavanaugh <erin@erin-MBA.local>
2016-03-24 21:34:04 -0700

    Pulling HTML from this.document

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -6,17 +6,30 @@
 // But instead we're going to implement it from scratch:
 var getElementsByClassName = function(className){
 	var results = []; 
-	var allHTML = $("html").html(); 
-	var elementList = allHTML.split("<");
-    for (var i = 0; i<elementList.length; i++) {
-    	if(elementList[i].indexOf(className) > 1) {
-    		console.log(elementList[i]); 
-    		var element = elementList[i].split(" ")[0];
-    		results.push(element+"."+className); 
-    	} 
-    }
+	// var allHTML = $("html").html(); 
+	var docs = this.document; 
+	var doc = this.document.childNodes; 
+	if(doc[1].className ==="") {
+		console.log("blank"); 
+	}
+	console.log("full doc", doc[1].className, "doc", doc[1], "methods", doc[0], "this");  
+	// console.log(allHTML); 
+	// var element = $("<div>").get(); 
+	// console.log(element); docs[1].html.className
 
-	console.log(results); 
+
+
+
+
+	// var elementList = allHTML.split("<");
+ //    for (var i = 0; i<elementList.length; i++) {
+ //    	if(elementList[i].indexOf(className) > 1) {
+ //    		var element = elementList[i].split(" ")[0];
+ //    		results.push(element+"."+className); 
+ //    	} 
+ //    }
+
+	// console.log(results); 
 }; 
 
 
