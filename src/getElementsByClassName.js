9e2d1e070eb867a29b65b5f937ec722aefbd1d3d
Erin Kavanaugh <erin@erin-MBA.local>
2016-03-28 21:14:15 -0700

    getElementsByClassName passing

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,51 +4,30 @@
 // };
 
 // But instead we're going to implement it from scratch:
+// var getElementsByClassName = function(className){
+	
 var getElementsByClassName = function(className){
-	// var allHTML = $("html").html(); 
-	// var doc = this.document; 
 	var results = [];  
-	var object = this.document.children[0].children; 
+	var object = this.document.children
 
-	// } else if (object.className === className) {
-	// 	results.push(object.className); 
-	// } 
-	// console.log(object); 
-	// for(var i = 0; i < object.length; i++) { 
-	// 	if(object[i].length < 1 || undefined) {
-	// 		return; 
-	// 	} else {
-	// 		console.log("no children",object[i]);
-	// 		return getElementsByClassName.call(object[i], className); 
-	// 	}
-	// }
-	for(var i = 0; i <object.length; i++) {
-		console.log("object[i]", object[i]);
-		console.log("children", object[i].children); 
-		if(object[i] !== undefined || object[i].children !== undefined) {
-			results.push(lookIntoColl(object[i], className));; 
-		}
-	}
-	console.log("results", results); 
-	return results; 
-	// return getChildNodes(object);  
-}; 
-
-function lookIntoColl (item, className) {
-	
-			if (item.className === className) {
-				return item; 
-			} else {
-				if(item.children) {
-					return lookIntoColl(item.children, className); 
+	function elementFinder(item, name) {
+		for (var i = 0; i<item.length; i++) {
+			if(item[i] !== undefined && item[i].children !== undefined) {
+				if(item[i].classList.length > 1) { 
+					for(var j =0; j< item[i].classList.length; j++) {
+						if(item[i].classList[j] === name) {
+							results.push(item[i]); 
+						}
+					}
+				} else if(item[i].className === name) { 
+					results.push(item[i]); 
 				}
-			} 
+				elementFinder(item[i].children, name); 
+			}
 		}
-	
-	
+	};
 
-
-
-
-
+	elementFinder(object, className); 
+	return results;  
+}; 
 // [body.targetClassName, div.targetClassName] "expectedArray"
